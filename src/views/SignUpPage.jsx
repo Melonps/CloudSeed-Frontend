30bdca6c965ed3cc8b82e0ef798967a0b9184b9c
Melops <95263124+Melonps@users.noreply.github.com>
2023-06-18 02:01:12 +0900

    [FIX] デザインコンポーネントの追加

--- src/views/SignUpPage.jsx
+++ src/views/SignUpPage.jsx
@@ -1,8 +1,10 @@
 import SignUp from "../components/SignUp";
+import { MainLogoDark } from "../components/Icon";
 
 const SignUpPage = () => {
     return (
         <>
+            <MainLogoDark className="w-72 xl:w-full h-24 xl:h-40 mx-auto mb-8" />
             <SignUp />
         </>
     );
