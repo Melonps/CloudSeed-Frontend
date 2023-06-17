68e2de4f795c5b5122cf73343b5689ac29f5ff31
Melops <95263124+Melonps@users.noreply.github.com>
2023-06-17 11:38:39 +0900

    [ADD] LIFFの導入、テスト用のコンポーネントを追加

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,19 +1,48 @@
 import SignOut from "../components/SignOut";
 import { Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import liff from "@line/liff";
+import "../App.css";
 
-const Home = () => {
+function Home() {
+    const [message, setMessage] = useState("");
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        liff.init({
+            liffId: import.meta.env.VITE_LIFF_ID,
+        })
+            .then(() => {
+                setMessage("LIFF init succeeded.");
+            })
+            .catch((e) => {
+                setMessage("LIFF init failed.");
+                setError(`${e}`);
+            });
+    });
+
     return (
-        <>
-            <>
-                <div>
-                    <Typography component="h1" variant="h5">
-                        Home
-                    </Typography>
-                    <SignOut />
-                </div>
-            </>
-        </>
+        <div className="App">
+            <h1>create-liff-app</h1>
+            {message && <p>{message}</p>}
+            {error && (
+                <p>
+                    <code>{error}</code>
+                </p>
+            )}
+            <a
+                href="https://developers.line.biz/ja/docs/liff/"
+                target="_blank"
+                rel="noreferrer"
+            >
+                LIFF Documentation
+            </a>
+            <Typography component="h1" variant="h5">
+                Home
+            </Typography>
+            <SignOut />
+        </div>
     );
-};
+}
 
 export default Home;
