95295294b47580528a956679a937a518f6137cd8
kkitagawaa <kitarira_1@yahoo.co.jp>
2023-06-18 11:27:50 +0900

    画像表示まで?

--- src/components/ShowImage.jsx
+++ src/components/ShowImage.jsx
@@ -6,22 +6,48 @@ const ShowImage = ({ words }) => {
     const [image, setImage] = useState(null)
     const [imageCreated, setImageCreated] = useState(false)
 
+    
+
     const postData = {
         "kw_list": words,
     }
 
     const fetchNewImage = async () => {
+        const headers = {
+            'Content-Type': 'application/json',
+        }
+
         try {
-            const response = await axios.post('https://cymntn2bea.execute-api.ap-northeast-3.amazonaws.com/dev/word-cloud', postData)
-                .then(response => response.blob())
-                .then(blob => {
-                    const reader = new FileReader();
-                    reader.onloadend = () => {
-                        setImage(reader.result)
-                    }
-                    reader.readAsDataURL(blob)
-                    setImageCreated(true)
-                })
+            // const response = await axios.post('https://cymntn2bea.execute-api.ap-northeast-3.amazonaws.com/dev/word-cloud', postData) { responseType: 'blob', timeout: 10000 }
+            const response = await axios.post('http://127.0.0.1:5000/word-cloud', postData, {
+                headers: headers,
+                responseType: 'blob'
+            })
+            const blob = response.data
+            // const blob = new Blob([response.data], { type: 'image/png' });
+            setImage(URL.createObjectURL(blob))
+            setImageCreated(true)
+            console.log(blob)
+            console.log(image)
+            // const blob = new Blob([response.data]);
+            // const reader = new FileReader();
+            // reader.onloadend = () => {
+            //     setImage(reader.result);
+            // };
+            // reader.readAsDataURL(blob);
+            // setImageCreated(true);
+            // console.log(image)
+                // .then(response => response.blob())
+                // .then(blob => {
+                //     const reader = new FileReader();
+                //     reader.onloadend = () => {
+                //         setImage(reader.result)
+                //     }
+                //     reader.readAsDataURL(blob)
+                //     setImageCreated(true)
+                // })
+                // const response = await axios.get('http://127.0.0.1:5000/word-cloud')
+                    // .then(response => console.log(response))
         } catch (error) {
             console.error(error);
         }
